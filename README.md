1320a57f722c84f3d1db0fb970f8103a03f636f0
Stiliyan Vasilev <stiliyan.vasilev@sap.com>
2020-03-26 00:22:26 +0200

    add monitor api call

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # paysafe-api-client
 
 Spring Boot project that integrates PaySafe API. The program is able to execute requests to the [Typical API Calls](https://developer.paysafe.com/en/classic-apis/3ds/typical-api-calls/verify-that-the-service-is-accessible/) endpoints.
+
+## TODO - add docs for the api calls
--- src/main/java/com/paysafe/client/paysafeapiclient/controllers/ServiceMonitorController.java
+++ src/main/java/com/paysafe/client/paysafeapiclient/controllers/ServiceMonitorController.java
@@ -1,6 +1,7 @@
 package com.paysafe.client.paysafeapiclient.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +9,21 @@ import org.springframework.web.client.RestTemplate;
 import com.paysafe.client.paysafeapiclient.models.Monitor;
 
 @RestController
+@RequestMapping("/paysafeclient")
 public class ServiceMonitorController {
+	
+	@Value("${paysafe.service.baseurl}")
+	private String serviceBaseUrl;
+	
+	@Value("${paysafe.service.monitor}")
+	private String serviceMonitor;
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@RequestMapping("/verifyservice")
 	public String getServiceStatus() {
-		Monitor monitor = restTemplate.getForObject("https://api.test.paysafe.com/threedsecure/monitor", Monitor.class);
+		Monitor monitor = restTemplate.getForObject(serviceBaseUrl.concat(serviceMonitor), Monitor.class);
 		return "3DS Service Status: " + monitor.getStatus();
 	}
 }
