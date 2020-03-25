57a1b46d425bcf0c7da4310a8ecd21d02beef58d
Stiliyan Vasilev <stiliyan.vasilev@sap.com>
2020-03-25 23:00:46 +0200

    Initial commit - project setup

---
+++ src/main/java/com/paysafe/client/paysafeapiclient/PaysafeApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.paysafe.client.paysafeapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysafeApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaysafeApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paysafe/client/paysafeapiclient/PaysafeApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paysafe.client.paysafeapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysafeApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
