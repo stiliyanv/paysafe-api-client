84fb52176db3326f6d6ed25feb13c73a8caa0109
Stiliyan Vasilev <stiliyan.vasilev@sap.com>
2020-03-27 08:40:01 +0200

    submit progress

--- src/main/java/com/paysafe/client/paysafeapiclient/controllers/AccountController.java
+++ src/main/java/com/paysafe/client/paysafeapiclient/controllers/AccountController.java
@@ -1,8 +1,16 @@
 package com.paysafe.client.paysafeapiclient.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/accounts")
 public class AccountController {
-
+	
+	@Value("${test.account.id}")
+	private String testAccountId;
+	
+	@Value("${test.user.api.key}")
+	private String testUserApiKey;
 }
