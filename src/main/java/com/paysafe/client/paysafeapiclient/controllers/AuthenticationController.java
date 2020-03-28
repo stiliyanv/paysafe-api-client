be67ffbb72275e9638d21c1dd9ee9c2d5fa17d84
Stiliyan Vasilev <stiliyan.vasilev@sap.com>
2020-03-28 23:59:51 +0200

    working progress

--- src/main/java/com/paysafe/client/paysafeapiclient/controllers/AuthenticationController.java
+++ src/main/java/com/paysafe/client/paysafeapiclient/controllers/AuthenticationController.java
@@ -1,7 +1,7 @@
 package com.paysafe.client.paysafeapiclient.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,15 +11,18 @@ import com.paysafe.client.paysafeapiclient.services.EnrollmentCheckService;
 @RestController
 public class AuthenticationController {
 	
+	@Value("${test.account.id}")
+	private String testAccountId;
+	
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Autowired
 	private EnrollmentCheckService enrollmentCheckService;
-	
-	@RequestMapping("/accounts/{id}/enrollmentchecks/e067d83f-add7-4b6b-8884-dfa8693dde1f/authentications")
-	public String getThreeDResult(@PathVariable String id) {
 		
-		return "todo";
+	// TODO
+	@RequestMapping("")
+	public void todo() {
+		
 	}
 }
